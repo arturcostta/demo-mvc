8996c6ffed63749e904affc95dc25bcf795baa65
Artur Costa <arturcostta@gmail.com>
2019-04-25 20:07:57 -0300

    Inclusao de cargo

---
+++ src/main/java/com/example/demo/converter/StringToDepartamentoConverter.java
@@ -0,0 +1,24 @@
+package com.example.demo.converter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.domain.Departamento;
+import com.example.demo.service.DepartamentoService;
+
+@Component
+public class StringToDepartamentoConverter implements Converter<String, Departamento>{
+
+	@Autowired
+	DepartamentoService service;
+	
+	@Override
+	public Departamento convert(String source) {
+		if(source.isEmpty()) {
+			return null;
+		}
+		return service.buscarPorId(Long.valueOf(source));
+	}
+	
+}
--- src/main/java/com/example/demo/web/controller/CargoController.java
+++ src/main/java/com/example/demo/web/controller/CargoController.java
@@ -1,20 +1,65 @@
 package com.example.demo.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.domain.Cargo;
+import com.example.demo.domain.Departamento;
+import com.example.demo.service.CargoService;
+import com.example.demo.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 
+	@Autowired
+	private CargoService service;
+	
+	@Autowired
+	private DepartamentoService departamentoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		service.salvar(cargo);
+		attr.addAttribute("success", "Cargo incluído com sucesso!");
+		return "redirect:/cargos/cadastrar";
+	}
+	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", service.buscarTodos());
 		return "/cargo/lista";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", service.buscarPorId(id));
+		return "cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		service.editar(cargo);
+		attr.addFlashAttribute("sucess", "Cargo editado com sucesso");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDepartamento(){
+		return departamentoService.buscarTodos();
 	}
 }
